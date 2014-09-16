dc1f86782b6b9c6a2babc2d658386151db2d9b21
Kevin Bond <kevinbond@gmail.com>
2014-09-16 12:57:56 -0400

    initial commit

---
+++ src/string.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @author Kevin Bond <kevinbond@gmail.com>
+ */
+
+namespace zenstruck\string;
+
+/**
+ * Replaces "&nbsp;" with a single space and converts multiple sequential spaces into a single space
+ *
+ * @param string $str
+ *
+ * @return string
+ */
+function remove_whitespace($str)
+{
+    return preg_replace('/\s+/', ' ', str_replace('&nbsp;', ' ', $str));
+}
+
+/**
+ * Similar to core "trim" but returns null instead of an empty string
+ * When an array is passed, all elements get processed recursively
+ *
+ * @param string|array $data
+ * @param null|string  $character_mask
+ *
+ * @return array|null|string
+ */
+function null_trim($data, $character_mask = null)
+{
+    if (!is_array($data)) {
+        return null === $character_mask ? trim($data) : trim($data, $character_mask) ?: null;
+    }
+
+    return array_map(
+        function ($value) use ($character_mask) {
+            return null_trim($value, $character_mask);
+        },
+        $data
+    );
+}
+
+/**
+ * Truncates text to a length without breaking words
+ *
+ * @param string $str
+ * @param int    $length
+ * @param string $suffix
+ *
+ * @return string
+ */
+function truncate_word($str, $length = 255, $suffix = '...')
+{
+    $output = remove_whitespace(trim($str));
+
+    if (strlen($output) > $length) {
+        $output = wordwrap($output, $length - strlen($suffix));
+        $output = substr($output, 0, strpos($output, "\n"));
+        $output .= $suffix;
+    }
+
+    return strlen($output) > $length ? '' : $output;
+}
---
+++ tests/StringTest.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace zenstruck\string;
+
+/**
+ * @author Kevin Bond <kevinbond@gmail.com>
+ */
+class StringTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider remove_whitespace_provider
+     */
+    function test_remove_whitespace($str, $expected)
+    {
+        $this->assertSame($expected, remove_whitespace($str));
+    }
+
+    /**
+     * @dataProvider null_trim_provider
+     */
+    function test_null_trim($data, $character_mask, $expected)
+    {
+        $this->assertSame($expected, null_trim($data, $character_mask));
+    }
+
+    /**
+     * @dataProvider truncate_word_provider
+     */
+    function test_truncate_word($str, $length, $suffix, $expected)
+    {
+        $this->assertSame($expected, truncate_word($str, $length, $suffix));
+    }
+
+    function remove_whitespace_provider()
+    {
+        return array(
+            array('foo', 'foo'),
+            array('foo    bar', 'foo bar'),
+            array('foo &nbsp;   bar', 'foo bar'),
+            array("  foo &nbsp;   \n\n\n  \r  bar", ' foo bar')
+        );
+    }
+
+    function null_trim_provider()
+    {
+        return array(
+            array(null, null, null),
+            array('foo', null, 'foo'),
+            array('  foo', null, 'foo'),
+            array('foo  ', null, 'foo'),
+            array('  foo  ', null, 'foo'),
+            array('foo / ', '/ ', 'foo'),
+            array('/  foo  ', ' /', 'foo'),
+            array('', null, null),
+            array(' ', null, null),
+            array('  ', null, null),
+            array(
+                array(' ', 'foo', null, '  foo', array('foo', '', ' ')),
+                null,
+                array(null, 'foo', null, 'foo', array('foo', null, null))
+            ),
+            array(
+                array(' /', 'foo/', '/', '  /foo', array('foo /', '/', '   / ')),
+                ' /',
+                array(null, 'foo', null, 'foo', array('foo', null, null))
+            ),
+            array(
+                array('foo' => '   bar  ', 'bar' => '     '),
+                null,
+                array('foo' => 'bar', 'bar' => null)
+            )
+        );
+    }
+
+    function truncate_word_provider()
+    {
+        return array(
+            array(null, 255, '', ''),
+            array('', 255, '', ''),
+            array('', 255, '...', ''),
+            array('foo', 3, '...', 'foo'),
+            array('foo', 2, '', ''),
+            array('foo', 2, '...', ''),
+            array('foo bar', 3, '', 'foo'),
+            array('foo bar baz', 6, '', 'foo'),
+            array('foo bar baz', 7, '', 'foo bar'),
+            array('foo bar baz', 7, '...', 'foo...'),
+            array('foo bar baz', 9, '...', 'foo...'),
+            array('foo bar baz', 10, '...', 'foo bar...'),
+            array('foo bar baz', 11, '...', 'foo bar baz'),
+            array('foo bar baz bob', 11, '...', 'foo bar...'),
+            array('foo bar baz', 12, '...', 'foo bar baz'),
+            array('      foo       bar  baz', 10, '...', 'foo bar...')
+        );
+    }
+}
